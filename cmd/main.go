abdc79f34fd390ea69c14ae787de400043aa7b4d
Azin Khan <azin@azinkhan.com>
2021-10-31 11:05:27 +0000

    Add flag for editing the feeds list
    This simply launches vim under the hood, allowing the user to edit the
    feed files.

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
 	"text/tabwriter"
 	"time"
 
@@ -16,14 +17,24 @@ const (
 
 func main() {
 	var dm, maxHours int
+	var edit bool
 	flag.IntVar(&dm, "m", 0, "Display mode")
 	flag.IntVar(&maxHours, "h", 24, "Max age of items (hours)")
+	flag.BoolVar(&edit, "e", false, "Edit the feeds list")
 	flag.Parse()
 	displayMode := rss.ReverseChronological
 	if dm == 1 {
 		displayMode = rss.Grouped
 	}
 	maxAge := time.Duration(maxHours) * time.Hour
+
+	if edit {
+		err := editFeedsFile(feedsFile)
+		if err != nil {
+			fmt.Println(err)
+		}
+		return
+	}
 
 	f, err := os.Open(feedsFile)
 	if err != nil {
@@ -38,4 +49,12 @@ func main() {
 	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
 	rss.Display(w, feedItems, displayMode)
 	w.Flush()
+}
+
+func editFeedsFile(filepath string) error {
+	cmd := exec.Command("vim", filepath)
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
 }

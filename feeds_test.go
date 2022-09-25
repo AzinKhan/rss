8c9ad570504af1447312b8816ee4b0e253b18d17
Azin Khan <azin@azinkhan.com>
2022-09-25 11:25:23 +0100

    Add some more filtering tests

--- feeds_test.go
+++ feeds_test.go
@@ -1,6 +1,7 @@
 package rss
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
@@ -44,10 +45,124 @@ func TestFiltersApplyOldestItem(t *testing.T) {
 
 		t.Run(tc.name, func(t *testing.T) {
 			result := tc.filters.Apply(tc.item)
-			if result != tc.expected {
-				t.Fail()
+			assertEqual(t, tc.expected, result)
+		})
+
+	}
+}
+
+func TestFilterMultiple(t *testing.T) {
+	type testcase struct {
+		item     FeedItem
+		expected bool
+	}
+	testcases := []struct {
+		name   string
+		filter Filter
+		cases  []testcase
+	}{
+		{
+			name:   "Max items",
+			filter: MaxItems(2),
+			cases: []testcase{
+				{
+					expected: true,
+					item:     FeedItem{},
+				},
+				{
+					expected: true,
+					item:     FeedItem{},
+				},
+				{
+					expected: false,
+					item:     FeedItem{},
+				},
+				{
+					expected: false,
+					item:     FeedItem{},
+				},
+			},
+		},
+		{
+			name:   "Max items per channel",
+			filter: MaxItemsPerChannel(1),
+			cases: []testcase{
+				{
+					expected: true,
+					item: FeedItem{
+						Channel: "1",
+					},
+				},
+				{
+					expected: true,
+					item: FeedItem{
+						Channel: "2",
+					},
+				},
+				{
+					expected: false,
+					item: FeedItem{
+						Channel: "1",
+					},
+				},
+				{
+					expected: false,
+					item: FeedItem{
+						Channel: "2",
+					},
+				},
+			},
+		},
+		{
+			name:   "Deduplicate",
+			filter: Deduplicate(),
+			cases: []testcase{
+				{
+					expected: true,
+					item: FeedItem{
+						Links: []string{"link1"},
+					},
+				},
+				{
+					expected: false,
+					item: FeedItem{
+						Links: []string{"link1"},
+					},
+				},
+				{
+					expected: false,
+					item: FeedItem{
+						Links: []string{"link1"},
+					},
+				},
+				{
+					expected: false,
+					item: FeedItem{
+						Links: []string{"link1"},
+					},
+				},
+			},
+		},
+	}
+
+	t.Parallel()
+	for _, tc := range testcases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			for _, c := range tc.cases {
+				result := tc.filter(c.item)
+				assertEqual(t, c.expected, result)
 			}
 		})
 
 	}
+}
+
+func assertEqual(t *testing.T, expected interface{}, result interface{}) {
+	if reflect.DeepEqual(expected, result) {
+		return
+	}
+	t.Fail()
+	t.Logf("Expected %v, got %v", expected, result)
 }
